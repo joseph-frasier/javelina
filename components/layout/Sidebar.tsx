8216dab65b08f8c0b31704199042fa0e65fd428a
Andrew-frasier <Andrew.frasier@irongrove.com>
2025-10-09 14:18:10 -0400

    Fix sidebar scrolling issue
    - Add min-h-0 to nav element to enable proper scrolling
    - Fixes issue where expanded orgs/environments couldn't scroll to bottom
    - Keeps sidebar sticky while allowing content to scroll within

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -90,7 +90,7 @@ export function Sidebar() {
       )}
 
       {/* Tree View */}
-      <nav className="flex-1 overflow-y-auto p-4">
+      <nav className="flex-1 overflow-y-auto p-4 min-h-0">
         {isCollapsed ? (
           // Collapsed view - show icons only
           <div className="flex flex-col space-y-2">
