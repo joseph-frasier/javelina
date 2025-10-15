f13b1ccca940c55bc6dcfa99f9fdaca007a6ba88
Andrew-frasier <Andrew.frasier@irongrove.com>
2025-10-15 15:28:48 -0400

    fix: add type assertion for Recharts data prop
    - Cast data to 'any' to satisfy Recharts type requirements
    - Recharts expects data with index signature which RecordTypeCount doesn't have
    - Fixes: Type 'RecordTypeCount[]' is not assignable to type 'ChartDataInput[]'

--- components/dns/RecordDistributionChart.tsx
+++ components/dns/RecordDistributionChart.tsx
@@ -15,7 +15,7 @@ export function RecordDistributionChart({ data }: RecordDistributionChartProps) 
       <ResponsiveContainer width="100%" height="100%">
         <PieChart>
           <Pie
-            data={data}
+            data={data as any}
             cx="50%"
             cy="50%"
             labelLine={false}
