28d1b654708060995fae243a738cc5d939c5a2ac
Andrew-frasier <Andrew.frasier@irongrove.com>
2025-11-26 09:03:47 -0500

    Change collapse icon to filled caret/arrow style

--- components/ui/CollapsibleCard.tsx
+++ components/ui/CollapsibleCard.tsx
@@ -103,17 +103,11 @@ export function CollapsibleCard({
         >
           <svg
             ref={chevronRef}
-            className="w-5 h-5 text-gray-500 dark:text-gray-400"
-            fill="none"
-            stroke="currentColor"
+            className="w-4 h-4 text-gray-500 dark:text-gray-400"
+            fill="currentColor"
             viewBox="0 0 24 24"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M19 9l-7 7-7-7"
-            />
+            <path d="M7 10l5 5 5-5H7z" />
           </svg>
         </button>
       }
