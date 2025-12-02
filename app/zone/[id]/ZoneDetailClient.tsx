8b8900cb1262be5a714e38ffece60845911d8fa0
Andrew-frasier <Andrew.frasier@irongrove.com>
2025-12-02 08:32:16 -0500

    Make Export button same color as Reload button in zone detail

--- app/zone/[id]/ZoneDetailClient.tsx
+++ app/zone/[id]/ZoneDetailClient.tsx
@@ -397,7 +397,7 @@ export function ZoneDetailClient({ zone, zoneId, organization, environment }: Zo
               </svg>
               Reload
             </Button>
-            <Button variant="secondary" size="sm" onClick={handleExport} className="justify-center">
+            <Button variant="primary" size="sm" onClick={handleExport} className="justify-center">
               <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
               </svg>
